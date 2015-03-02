132eb9bc3b52939d007947b01a1385bd37d17883
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-02 19:07:52 -0300

    fixing db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302194144) do
+ActiveRecord::Schema.define(version: 20150302195800) do
 
   create_table "companies", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "street_address"
+    t.string   "number_address"
+    t.string   "city"
+    t.string   "neighborhood"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
