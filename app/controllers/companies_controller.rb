89db2b8e1974ea7876e1ab2588c8a928745ac6e8
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-02 21:36:27 -0300

    creating actions and views, new, show index and edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,33 +1,42 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def show
+    @company = Company.find(params[:id])
   end
 
   def new
-    @companies = Company.new
+    @company = Company.new
   end
 
   def create
-    @companies = Company.new(companies_params)
-    if companies.save
-      redirect_to @companies
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
     else
       render "new"
     end  
   end
 
   def edit
+    @company = Company.find(params[:id])
   end
 
   def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to @company
+    else
+      render "new"
+    end
   end
 
   def destroy
   end
 
-  def companies_params
+  def company_params
     params.require(:company).permit(:name, :email, :phone, :street_address, :number_address, :city, :neighborhood)    
   end
 end
