becbfef7ff98baf5c32e7c8a9d34e21efeca6b79
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-03 21:09:03 -0300

    testing creat on companies controller

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe CompaniesController, type: :controller do
   describe "listing companies on index" do
     it "returns all companies" do
@@ -8,11 +9,20 @@ RSpec.describe CompaniesController, type: :controller do
       expect(assigns(:companies)).to match_array([company1, company2])
     end
   end    
+  
   describe "show a company" do
     it "return same company by id" do
       company = Company.create({name: "Empresa 5", email: "eu@eu.com", phone: "1141130853", street_address: "rua gloria", number_address: "86", city: "sao paulo", neighborhood: "vila prudente"})
       get :show, id: company.id
       expect(assigns(:company)).to eq(company)
+    end
+  end
+
+  describe "testing create" do
+    it "company create" do
+      post :create, company: {name: "Empresa Banana", email: "eu@eu.com", phone: "1141130853", street_address: "rua gloria", number_address: "86", city: "sao paulo", neighborhood: "vila prudente"}
+      expect(Company.last.name).to eq "Empresa Banana"
+      expect(response).to redirect_to action: :show, id: assigns(:company).id 
     end
   end
 end
