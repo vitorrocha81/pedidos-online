0b524ef2daefca36edc9461e8aa61d48f20657a2
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-04 19:25:41 -0300

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302195800) do
+ActiveRecord::Schema.define(version: 20150304221945) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -24,5 +24,23 @@ ActiveRecord::Schema.define(version: 20150302195800) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
+
+  create_table "company_admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "company_admins", ["email"], name: "index_company_admins_on_email", unique: true
+  add_index "company_admins", ["reset_password_token"], name: "index_company_admins_on_reset_password_token", unique: true
 
 end
