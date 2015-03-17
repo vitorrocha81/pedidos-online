142647f61852f65de4d05b06ce5a241ed06dfacf
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-17 20:04:47 -0300

    testing product category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313004949) do
+ActiveRecord::Schema.define(version: 20150317225824) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20150313004949) do
   add_index "company_admins", ["company_id"], name: "index_company_admins_on_company_id"
   add_index "company_admins", ["email"], name: "index_company_admins_on_email", unique: true
   add_index "company_admins", ["reset_password_token"], name: "index_company_admins_on_reset_password_token", unique: true
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "create" do
+    it "a valid category" do
+      category = ProductCategory.new({name: "categoria"})
+      expect(category.valid?).to be true
+    end
+  end
+
+  describe "don't create" do
+    it "an invalid category" do
+      category = ProductCategory.new
+      expect(category.valid?).to be false
+    end
+  end
 end
