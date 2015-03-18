74b4ea742b6916f12188f1dfca3ce40d6a35877f
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-17 21:37:24 -0300

    fixing view spec

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "products/index.html.erb", type: :view do
   let(:product) { instance_double("Product").as_null_object }
   it "renders index products" do
+    product.stub(:product_img) { nil }
     product.stub(:to_partial_path) {"products/product"}
     assign(:products, [product, product])
     render
