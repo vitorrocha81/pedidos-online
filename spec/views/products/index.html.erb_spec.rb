bf3f78cadf19b85c1ed46406e9d2ccec07b5d8f7
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-17 21:17:08 -0300

    partial product view

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "products/index.html.erb", type: :view do
+  let(:product) { instance_double("Product").as_null_object }
   it "renders index products" do
-    assign(:products, [instance_double("Product").as_null_object])
+    product.stub(:to_partial_path) {"products/product"}
+    assign(:products, [product, product])
     render
-    expect(view).to render_template(:index, :count => 1)
+    expect(view).to render_template(partial: "_product", :count => 2)
   end
 end
