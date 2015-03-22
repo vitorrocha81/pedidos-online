8ac4f5b65641510158c0dbbe1f4e1e68e9094843
Vitor Rocha <vitor.rocha81@bol.com.br>
2015-03-22 01:47:34 -0300

    view index test fixed

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
-
 RSpec.describe "products/index.html.erb", type: :view do
   let(:product) { instance_double("Product").as_null_object }
   it "renders index products" do
-    product.stub(:product_img) { nil }
-    product.stub(:product_category) { instance_double("ProductCategory", name: "nome")}
-    product.stub(:to_partial_path) {"products/product"}
+    allow(product).to receive(:product_img).and_return(nil)
+    allow(product).to receive(:product_category).
+      and_return(instance_double("ProductCategory", name: "nome"))
+    allow(product).to receive(:to_partial_path).and_return("products/product")
     assign(:products, [product, product])
     render
-    expect(view).to render_template(partial: "product", :count => 2)
+    expect(view).to render_template(partial: "_product", :count => 2)
   end
 end
